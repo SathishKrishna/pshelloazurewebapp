7e9b55e021447937da0ee84c56a6a8d4b8497a53
Sathish Krishnamoorthi <sathish.krishnamoorthi@salliemae.com>
2018-07-27 16:57:28 -0400

    first commit

---
+++ PSHelloAzureWebApp/Controllers/CoursesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PSHelloAzureWebApp.Models.Courses;
+using PSHelloAzureWebApp.Services;
+
+namespace PSHelloAzureWebApp.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly CourseStore courseStore;
+
+        public CoursesController(CourseStore courseStore)
+        {
+            this.courseStore = courseStore;
+        }
+        public IActionResult Index()
+        {
+            var model = courseStore.GetAllCourses();
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> Insert()
+        {
+            var data = new SampleData().GetCourses();
+            await courseStore.InsertCourses(data);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ PSHelloAzureWebApp/Services/CourseStore.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Azure.Documents.Client;
+using PSHelloAzureWebApp.Models.Courses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PSHelloAzureWebApp.Services
+{
+    public class CourseStore
+    {
+        private DocumentClient documentClient;
+        private Uri coursesLink;
+
+        public CourseStore()
+        {
+            var uri = new Uri("https://scaryworks.documents.azure.com:443/");
+            var accountKey = "nZ8QolIeRI33T7QbAu6hiNUKCEEAhDoJpDRxPZOhouRF0aPHOKwLTN7LbQKHO1M3mXjiHESCRHlKQrBCDl6BrQ==";
+            documentClient = new DocumentClient(uri, accountKey);
+            coursesLink = UriFactory.CreateDocumentCollectionUri("pshelloazure", "courses");
+        }
+
+        public async Task InsertCourses(IEnumerable<Course> courses)
+        {
+            foreach (var course in courses)
+            {
+                await documentClient.CreateDocumentAsync(coursesLink, course);
+            }
+        }
+
+        public IEnumerable<Course> GetAllCourses()
+        {
+            var courses = documentClient.CreateDocumentQuery<Course>(coursesLink).OrderBy(c => c.Title);
+            return courses;
+        }
+    }
+}
